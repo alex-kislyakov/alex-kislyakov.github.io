5e970c08dc172501e4adaf1530dfce1640bfe5a6
AlexFreik <alexfreik5@gmail.com>
2022-11-27 17:33:44 +0100

    transfer blogs to zen

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,140 +1,152 @@
----
----
-
 const HIGH_MATCH_BOUNDARY = -20;
 const LOW_MATCH_BOUNDARY = -500;
 
 var searchInputElems;
 var sections;
 
+class Post {
+	title;
+	url;
+	tags;
+
+	constructor(title, url, tags) {
+		this.title = title;
+		this.url = url;
+		this.tags = tags === '' ? [] : tags.split(' ');
+	}
+}
+
+const posts = [
+	new Post(
+		'Cмысл восхождения в горы',
+		'https://dzen.ru/a/YuRIU4msRXyJHeVk',
+		'жизнь путешествия походкавказ походтурция ликийскаятропа походкипр походкрым',
+	),
+	new Post(
+		'Свадхистана чакра',
+		'https://dzen.ru/a/Y4N58rmSVxp6Nk3e',
+		'йога медитация осознанность психология',
+	),
+];
+
+function getInputTags() {
+	const val = searchInputElems['tags'].value;
+	if (val === '') return [];
+	return val.split(' ');
+}
+
+function getInputName() {
+	return searchInputElems['name'].value;
+}
 
 function search() {
-    sections['blogs-list'].innerHTML = getPostsSectionHTML(getPostsList());
+	sections['blogs-list'].innerHTML = getPostsSectionHTML(getPostsList());
 }
 
 function assignInputElements() {
-    searchInputElems = {
-        'tags': document.getElementById('tags'),
-        'name': document.getElementById('name'),
-    };
+	searchInputElems = {
+		tags: document.getElementById('tags'),
+		name: document.getElementById('name'),
+	};
 }
+
 function assignSectionElements() {
-    sections = {
-        'blogs-list': document.getElementById('blogs-list'),
-    }
+	sections = {
+		'blogs-list': document.getElementById('blogs-list'),
+	};
 }
+
 function clearFields() {
-    searchInputElems['tags'].value = '';
-    searchInputElems['name'].value = '';
-    search();
+	searchInputElems['tags'].value = '';
+	searchInputElems['name'].value = '';
+	search();
 }
 
-
-function isIncludes(noteVal, userVal) {
-    if (userVal == null || userVal == '') {
-        return true;
-    }
-    return userVal.includes(noteVal);
-}
-function getFuzzyMatchScore(noteVal, userVal) {
-    if (userVal == null || userVal === '') {
-        return 0;
-    }
-    var results = fuzzysort.go(userVal, noteVal.split('|'));
-    return (results.length == 0 
-        ? Number.NEGATIVE_INFINITY : results[0]['score']);
+function getFuzzyMatchScore(post, inputName) {
+	if (inputName === '') return 0;
+	const results = fuzzysort.go(inputName, [post.title]);
+	return results.length === 0 ? Number.NEGATIVE_INFINITY : results[0]['score'];
 }
 
 function getPostHTML(post) {
-    return '\
+	return (
+		'\
     <li>\
-        <span class="post-meta">' + post['date'] + '</span>\
+        <span class="post-meta">' +
+		'date' +
+		'</span>\
         <h3>\
-            <a class="post-link" href="' + post['url'] + '">' +
-                post['title'] +
-            '</a>\
-        </h3>\
-    </li>';
+            <a class="post-link" href="' +
+		post.url +
+		'">' +
+		post.title +
+		'</a>\
+    </h3>\
+</li>'
+	);
 }
+
 function getMatchingGroups(elementsList) {
-    let matchingGroups = {'highMatch': '', 'medMatch': '', 'lowMatch': ''};
-    elementsList.sort(function(first, second) { return second[0] - first[0]; });
-    elementsList.forEach(function(item, index, array) {
-        if (item[0] >= HIGH_MATCH_BOUNDARY) {
-            matchingGroups['highMatch'] += item[1];
-        } else if (item[0] > LOW_MATCH_BOUNDARY) {
-            matchingGroups['medMatch'] += item[1];
-        } else {
-            matchingGroups['lowMatch'] += item[1];
-        }
-    })
-    return matchingGroups;
+	let matchingGroups = { highMatch: '', medMatch: '', lowMatch: '' };
+	elementsList.sort(function (first, second) {
+		return second[0] - first[0];
+	});
+	elementsList.forEach(function (item, index, array) {
+		if (item[0] >= HIGH_MATCH_BOUNDARY) {
+			matchingGroups['highMatch'] += item[1];
+		} else if (item[0] > LOW_MATCH_BOUNDARY) {
+			matchingGroups['medMatch'] += item[1];
+		} else {
+			matchingGroups['lowMatch'] += item[1];
+		}
+	});
+	return matchingGroups;
 }
+
 function getPostsSectionHTML(postsList) {
-    const notesGroups = getMatchingGroups(postsList);
-    let hmNum = notesGroups['highMatch'].length,
-        mmNum = notesGroups['medMatch'].length,
-        lmNum = notesGroups['lowMatch'].length;
-    let blogsSectionHTML = '';
-    if (hmNum !== 0 && (mmNum + lmNum) !== 0) {
-        blogsSectionHTML += '<hr><h2>Высокое совпадение</h2>';
-    }
-    blogsSectionHTML += notesGroups['highMatch'];
-    if (mmNum !== 0) {
-        blogsSectionHTML += '<hr><h2>Среднее совпадение</h2>';
-    }
-    blogsSectionHTML += notesGroups['medMatch'];
-    if (lmNum) {
-        blogsSectionHTML += '<hr><h2>Низкое совпадение</h2>';
-    }
-    blogsSectionHTML += notesGroups['lowMatch'];
-    return blogsSectionHTML;
+	const notesGroups = getMatchingGroups(postsList);
+	let hmNum = notesGroups['highMatch'].length,
+		mmNum = notesGroups['medMatch'].length,
+		lmNum = notesGroups['lowMatch'].length;
+	let blogsSectionHTML = '';
+	if (hmNum !== 0 && mmNum + lmNum !== 0) {
+		blogsSectionHTML += '<hr><h2>Высокое совпадение</h2>';
+	}
+	blogsSectionHTML += notesGroups['highMatch'];
+	if (mmNum !== 0) {
+		blogsSectionHTML += '<hr><h2>Среднее совпадение</h2>';
+	}
+	blogsSectionHTML += notesGroups['medMatch'];
+	if (lmNum) {
+		blogsSectionHTML += '<hr><h2>Низкое совпадение</h2>';
+	}
+	blogsSectionHTML += notesGroups['lowMatch'];
+	return blogsSectionHTML;
 }
 
-function isPostTagsMatching(postTags) {
-    let inputTags = searchInputElems['tags'].value.split(' ');
-    for (let i = 0; i < postTags.length; ++i) {
-        for (let j = 0; j < inputTags.length; ++j) {
-            if (postTags[i].includes(inputTags[j])) {
-                return true;
-            }
-        }
-    }
-    return false;
+function isPostMatching(post, inputTags) {
+	console.log(post, inputTags);
+	if (inputTags.length === 0) return true;
+	for (const inputTag of inputTags) {
+		for (const tag of post.tags) {
+			if (tag.includes(inputTag)) return true;
+		}
+	}
+	return false;
 }
 
-function isPostMatching(post) {
-    if (searchInputElems['tags'].value === '')
-        return true;
-    if (post['tags'] === null)
-        return false;
-    return isPostTagsMatching(post['tags']);
-}
 function getPostsList() {
-    let list = [];
-    let postEntry = {};
-    //{% for post in site.posts %}
-    //{%- assign date_format = site.minima.date_format | default: "%b %-d, %Y" -%}
-    //
-        postEntry = {
-            'tags': {{ post.mytags |  jsonify}},
-            'title': '{{ post.title | escape }}',
-            'categories': '{{ post.categories }}',
-            'date': '{{ post.date | date: date_format}}',
-            'url': '{{ post.url | relative_url }}',
-        }
-        if (isPostMatching(postEntry)) {
-            list.push([
-                getFuzzyMatchScore(postEntry['title'],
-                    searchInputElems['name'].value),
-                        getPostHTML(postEntry)]);
-        } 
-    //{% endfor %}
-    return list;
+	let list = [];
+	const inputTags = getInputTags();
+	for (const post of posts) {
+		if (isPostMatching(post, inputTags)) {
+			list.push([getFuzzyMatchScore(post, getInputName()), getPostHTML(post)]);
+		}
+	}
+	return list;
 }
 
 function showTags(tags) {
-    searchInputElems['tags'].value = tags;
-    search();
-}       
-
+	searchInputElems['tags'].value = tags;
+	search();
+}
